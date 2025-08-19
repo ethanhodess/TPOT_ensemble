9e922ced4f1eee2b199ba80cb0195a4fa1057d75
ethanhodess <ethanhodess@gmail.com>
2025-08-18 18:39:04 -0700

    round 2 results

--- collect/collect_csvs.py
+++ collect/collect_csvs.py
@@ -16,4 +16,4 @@ for f in files[1:]:
     dfs.append(df)
 
 final_df = pd.concat(dfs, ignore_index=True)
-final_df.to_csv("combined_results.csv", index=False)
+final_df.to_csv("combined_results_LR.csv", index=False)
