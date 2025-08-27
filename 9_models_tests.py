857df74df7a6c90059dd852de52170311fffdc0c
ethanhodess <ethanhodess@gmail.com>
2025-08-26 20:30:49 -0700

    rest of correlation updates

--- 9_models_tests.py
+++ 9_models_tests.py
@@ -40,14 +40,17 @@ def get_pipeline_space(seed):
 def set_up_estimators(pareto_front, X_train, y_train, X_test, y_test, seed):
     estimators = []
     voting_weights = []
-    top_80_estimators = []
+    top_half_estimators = []
     random_sample_estimators = []
     highest_accuracy = 0
+    best_estimator = None
+    diverse_estimators = []
+    corr_threshold = 0.90
+    voting_weights_diverse = []
 
     # setting values for top 50% and random sampling
-    #middle_row = pareto_front.shape[0] // 2
-    top_80_row = int(len(pareto_front) * 0.8)
-    top_80 = pareto_front.sort_values(by='balanced_accuracy_score', ascending=False).iloc[:top_80_row]
+    middle_row = pareto_front.shape[0] // 2
+    top_half = pareto_front.sort_values(by='balanced_accuracy_score', ascending=False).iloc[:middle_row]
 
     random_sample = pareto_front.sample(frac=0.5, random_state=seed)
 
@@ -59,17 +62,18 @@ def set_up_estimators(pareto_front, X_train, y_train, X_test, y_test, seed):
 
         if accuracy > highest_accuracy:
             highest_accuracy = accuracy
+            best_estimator = fitted_pipeline
 
         voting_weights.append(accuracy**40)
 
         #fitted_pipeline_tuple = ((str(i), fitted_pipeline))
         estimators.append(fitted_pipeline)
 
-    # creates top 80% primary objective (bacc) list
-    for i in range(len(top_80)):
-        fitted_pipeline = top_80.iloc[i, 10].fit(X_train, y_train)
+    # creates top 50% primary objective (bacc) list
+    for i in range(len(top_half)):
+        fitted_pipeline = top_half.iloc[i, 10].fit(X_train, y_train)
         #fitted_pipeline_tuple = ((str(i), fitted_pipeline))
-        top_80_estimators.append(fitted_pipeline)
+        top_half_estimators.append(fitted_pipeline)
 
     # creates random sample list
     for i in range(len(random_sample)):
@@ -77,7 +81,25 @@ def set_up_estimators(pareto_front, X_train, y_train, X_test, y_test, seed):
         fitted_pipeline_tuple = ((str(i), fitted_pipeline))
         random_sample_estimators.append(fitted_pipeline_tuple)
 
-    return estimators, top_80_estimators, random_sample_estimators, voting_weights, highest_accuracy
+    # creates diverse pipeline list
+    if best_estimator is not None:
+        best_preds = best_estimator.predict(X_test)
+        diverse_estimators.append(best_estimator)
+        voting_weights_diverse.append(highest_accuracy**40)
+
+        for est in estimators:
+            preds = est.predict(X_test)
+            if np.std(preds) == 0 or np.std(best_preds) == 0:  # avoid NaN corr
+                corr = 1.0 if np.all(preds == best_preds) else 0.0
+            else:
+                corr = np.corrcoef(best_preds, preds)[0, 1]
+
+            if corr < corr_threshold:
+                diverse_estimators.append(est)
+                acc = accuracy_score(y_test, preds)
+                voting_weights_diverse.append(acc**40)
+
+    return estimators, top_half_estimators, random_sample_estimators, voting_weights, highest_accuracy, diverse_estimators, voting_weights_diverse
 
 
 def vote_hard(estimators, X_test, weights=None):
@@ -153,19 +175,19 @@ def main():
             with open((f'pareto_front_{task_id}_#{run_num}.pkl'), "wb") as f:
                 pickle.dump(pf, f)
 
-        estimators, top_half_estimators, random_sample_estimators, voting_weights, individual_highest_accuracy = set_up_estimators(
+        estimators, top_half_estimators, random_sample_estimators, voting_weights, individual_highest_accuracy, diverse_estimators, voting_weights_diverse = set_up_estimators(
             pf, X_train, y_train, X_test, y_test, run_num)
 
         # Model 1: all, hard voting, weighted
-        results_1 = vote_hard(estimators=estimators, X_test=X_test, weights=voting_weights)
+        results_1 = vote_hard(estimators=diverse_estimators, X_test=X_test, weights=voting_weights_diverse)
         accuracy_1 = accuracy_score(y_test, results_1)
 
         # Model 2: all, soft voting, weighted
-        results_2 = vote_soft(estimators=estimators, X_test=X_test, weights=voting_weights)
+        results_2 = vote_soft(estimators=diverse_estimators, X_test=X_test, weights=voting_weights_diverse)
         accuracy_2 = accuracy_score(y_test, results_2)
 
         full_results.append({"task id": task_id,
-                            "run #": run_num,
+                             "run #": run_num,
                              "individual": individual_highest_accuracy,
                              "model 1": accuracy_1,
                              "model 2": accuracy_2
