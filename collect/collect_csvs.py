2ed135e818d2b288665e4cee3e6debcb7669e458
ethanhodess <53194842+ethanhodess@users.noreply.github.com>
2026-09-03 14:22:52 -0700

    bagging test versus base tpot

--- collect/collect_csvs.py
+++ collect/collect_csvs.py
@@ -2,8 +2,8 @@ import pandas as pd
 import glob
 
 # folder passed as argument
-path = "/common/hodesse/hpc_test/TPOT2_ensemble/logs/*.csv"
-
+#path = "/common/hodesse/hpc_test/TPOT2_ensemble/logs/*.csv"
+path = "/Users/ethanhodess/Documents/Documents - Ethan’s MacBook Pro/Cedars/2025/TPOT_ensemble/logs_random/*.csv"
 files = glob.glob(path)
 
 dfs = []
@@ -16,4 +16,4 @@ for f in files[1:]:
     dfs.append(df)
 
 final_df = pd.concat(dfs, ignore_index=True)
-final_df.to_csv("500_imbalanced_balacc.csv", index=False)
+final_df.to_csv("bagging_res_1.csv", index=False)
