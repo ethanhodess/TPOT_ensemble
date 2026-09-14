e1ea7407e4ed46ffc9b73c82b15e166c835ce711
Jose <jgh9094@gmail.com>
2026-09-14 08:21:47 -0700

    missing runs

--- random_ens.sh
+++ random_ens.sh
@@ -2,13 +2,13 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=12
-#SBATCH -t 1:00:00
+#SBATCH -t 3:00:00
 #SBATCH --mem=64GB
 #SBATCH --job-name=tpot-ensemble
 #SBATCH -p moore,defq
 #SBATCH --exclude=esplhpc-cp040
-#SBATCH -o ./logs/outputs/output.%j_%a.out # STDOUT
-#SBATCH --array=0-545
+#SBATCH -o ./logs_missing/outputs/output.%j_%a.out # STDOUT
+#SBATCH --array=6,63,65,68,323,326,328,329,333,480,521,523,526,527,528,530,531,534,539,540,544
 
 RUN=${SLURM_ARRAY_TASK_ID:-1}
 echo “Run: ${RUN}”
@@ -19,6 +19,6 @@ conda activate ethan
 echo RunStart
 srun -u python random_ensemble.py \
 --n_jobs 12 \
---savepath logs_random \
+--savepath logs_random_missing \
 --num_runs ${RUN} \
 --data_dir /home/hernandezj45/Repos/TPOT_ensemble/Raw_OpenML_Suite_271_Classification
